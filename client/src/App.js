351b9ad75346d9b164de7cf0161ffaed01e8f7fd
Vin274 <101485986+Vin274@users.noreply.github.com>
2022-09-17 23:54:00 +1200

    completed card component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-
+import "./App.css"
+import Card from "./components/Card/Card"
+import house from "./test/mock-data"
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    console.log(house)
+    return (
+        <div className="App">
+            {house.map((i, idx) => (
+                <Card item={i} key={idx} />
+            ))}
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ client/src/test/mock-data.js
@@ -0,0 +1,19 @@
+const house = [
+    {
+        header: "Apartment Living in Auckland",
+        address: "147-149 Victoria Street West, City Centre, Auckland",
+        bedroom: 2,
+        bathroom: 1.5,
+        parking: 1,
+        pet: "pet friendly",
+        cost: 500,
+        pictures: {
+            1: "https://trademe.tmcdn.co.nz/photoserver/plus/1828419644.jpg",
+            2: "https://trademe.tmcdn.co.nz/photoserver/plus/1819985370.jpg",
+            3: "https://trademe.tmcdn.co.nz/photoserver/plus/1829719313.jpg",
+            4: "https://trademe.tmcdn.co.nz/photoserver/plus/1826547210.jpg",
+        },
+    },
+]
+
+export default house
