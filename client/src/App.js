49baa63621eab849635cf25154effac9770fc7e6
Caleb Winters <calebw@missionreadyhq.com>
2022-09-19 12:06:54 +1200

    Fresh push

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css"
+import Listings from "./pages/Listings"
+import Card from "./components/Card/Card"
+import house from "./test/mock-data"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+    return (
+    <Router>
+        <div className="App">
+
+            <Routes>
+                <Route path="/" element={{/* Home Page component here */}}></Route>
+                <Route path="/propertyfeauture"element={{/* Feature Page component here */}}></Route>
+                <Route path="/propertylisting" element={<Listings />}> </Route>
+                <Route path="/booking" element={{/*Booking Page components here*/}}></Route>
+                <Route path="/applicationform" element={{/*Application Form Component here */}}></Route>
+            </Routes>
+
+        </div>
+    </Router>
+    )
+}
+
+export default App
---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,115 @@
+import "../../style/Card.css"
+import { useState } from "react"
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs"
+import { IoLocationOutline } from "react-icons/io5"
+import { TbBed, TbBath, TbPaw, TbHeart } from "react-icons/tb"
+import { RiCarLine } from "react-icons/ri"
+import dot from "./assets/dot.png"
+import selectedDot from "./assets/selected-dot.png"
+
+function Card({
+    item: { header, address, bedroom, bathroom, parking, pet, cost, pictures },
+}) {
+    const [currentImage, setCurrentImage] = useState(1)
+    const [heartColor, setHeartColor] = useState("")
+
+    const nextImage = () => {
+        if (currentImage !== Object.keys(pictures).length) {
+            return setCurrentImage(currentImage + 1)
+        } else {
+            if (currentImage === Object.keys(pictures).length) {
+                return setCurrentImage(1)
+            }
+        }
+    }
+
+    const prevImage = () => {
+        if (currentImage !== 1) {
+            return setCurrentImage(currentImage - 1)
+        } else {
+            if (currentImage === 1) {
+                return setCurrentImage(Object.keys(pictures).length)
+            }
+        }
+    }
+
+    const toggleHeart = () => {
+        heartColor === "" ? setHeartColor("#F85757") : setHeartColor("")
+    }
+
+    const moveDot = (index) => {
+        setCurrentImage(index)
+    }
+
+    return (
+        <>
+            <div className="card-container">
+                <div className="card-image">
+                    <span onClick={toggleHeart} className="card-image-heart">
+                        <TbHeart color={heartColor} size={30} />
+                    </span>
+                    <span className="left-arrow" onClick={prevImage}>
+                        <BsChevronLeft />
+                    </span>
+                    <img src={pictures[`${currentImage}`]} alt="house" />
+                    <span className="right-arrow" onClick={nextImage}>
+                        <BsChevronRight />
+                    </span>
+                </div>
+
+                <div className="card-description">
+                    <h3>{header}</h3>
+                </div>
+
+                <div className="card-address">
+                    <span className="card-description-logo">
+                        <IoLocationOutline size={30} />
+                    </span>
+                    <p>{address}</p>
+                </div>
+
+                <div className="card-icons">
+                    <span className="card-icons-bed">
+                        <TbBed size={35} />
+                        &nbsp;
+                        <p>{bedroom}</p>
+                    </span>
+                    <span className="card-icons-bath">
+                        <TbBath size={25} />
+                        &nbsp;
+                        <p>{bathroom}</p>
+                    </span>
+                    <span className="card-icons-car">
+                        <RiCarLine size={25} />
+                        &nbsp;
+                        <p>{parking}</p>
+                    </span>
+                    <span className="card-icons-pet">
+                        <TbPaw size={25} />
+                        &nbsp;
+                        <p>{pet}</p>
+                    </span>
+                </div>
+
+                <div className="card-price">
+                    <p>${cost} per week</p>
+                </div>
+                <span className="card-image-dot">
+                    {Object.values(pictures).map((item, index) => (
+                        <span key={index} onClick={() => moveDot(index + 1)}>
+                            <img
+                                src={
+                                    currentImage === index + 1
+                                        ? selectedDot
+                                        : dot
+                                }
+                                alt="dot"
+                            />
+                        </span>
+                    ))}
+                </span>
+            </div>
+        </>
+    )
+}
+export default Card
