f1440c78bf6d9b346e31ae128eb556ee98ef15a0
Wtipene <Wilemtipene@gmail.com>
2022-09-20 13:54:24 +1200

    Added hero image and search function to home page

---
+++ client/src/components/HomeSearch.js
@@ -0,0 +1,38 @@
+import {useContext} from 'react'
+import AppContext from '../context/AppContext'
+import '../style/HomeHeroImg.css'
+
+
+
+const HomeSearch = () => {
+  const {HomeData} = useContext(AppContext)
+
+  return (
+    <div className='searchContainer'>
+        <div className='searchSelect'>
+          <select>
+            <option> Property type</option>
+          </select>
+          <input type='text' placeholder='Search by location property ID...' ></input>
+        </div>
+        <div className='searchButton'>
+          <button>Search Now</button>
+          <p>Filters</p>
+        </div>
+        <div className='houseIcon'>
+          {HomeData.select}
+        </div>
+        <div className='mapPin'>
+          {HomeData.mappin}
+        </div>
+        <div className='homeSliders'>
+          {HomeData.sliders}
+        </div>
+        <div className='homeSearch'>
+          {HomeData.search}
+        </div>
+    </div>
+  )
+}
+
+export default HomeSearch
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,15 @@
 import Header from '../components/header/Header.js'
+import Footer from '../components/footer/Footer.js'
+import HomeHeroImg from '../components/HomeHeroImg.js'
+import HomeSearch from '../components/HomeSearch.js'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <HomeHeroImg />
+      <HomeSearch />
+      <Footer />
     </div>
   )
 }
