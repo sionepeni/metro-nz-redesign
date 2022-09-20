a7739f2f8ba201acfbe5e7239d2b3488d23c8acd
Caleb Winters <calebw@missionreadyhq.com>
2022-09-20 23:06:34 +1200

    adding page changes/updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css"
 import Listings from "./pages/Listings"
 import Home from "./pages/Home"
 import Card from "./components/Card/Card"
-import house from "./test/mock-data"
+// import house from "./test/mock-data"
 import PropertyFeature from "./pages/PropertyFeature"
 import {AppProvider} from "./context/AppContext"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -14,7 +14,7 @@ function App() {
             <Router>
                 <div className="App">
                     <Routes>
-<<<<<<< HEAD
+
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/propertyfeature"element={<PropertyFeature />}></Route>
                     <Route path="/propertylisting" element={<Listings />}> </Route>
@@ -22,44 +22,9 @@ function App() {
                     <Route path="/applicationform" element={{/*Application Form Component here */}}></Route>
                 </Routes>
             </div>
-        </Router>
-    </AppProvider>
-
-
-=======
-                        <Route path="/" element={<Home />}></Route>
-                        <Route
-                            path="/propertyfeauture"
-                            element={
-                                {
-                                    /* Feature Page component here */
-                                }
-                            }
-                        ></Route>
-                        <Route path="/propertylisting" element={<Listings />}>
-                            {" "}
-                        </Route>
-                        <Route
-                            path="/booking"
-                            element={
-                                {
-                                    /*Booking Page components here*/
-                                }
-                            }
-                        ></Route>
-                        <Route
-                            path="/applicationform"
-                            element={
-                                {
-                                    /*Application Form Component here */
-                                }
-                            }
-                        ></Route>
-                    </Routes>
-                </div>
-            </Router>
+            </Router>                
         </AppProvider>
->>>>>>> 5bb7fe38b7a1f933e7579cef02658590d189816e
+
     )
 }
 
