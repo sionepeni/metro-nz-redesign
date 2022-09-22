4c6ac036e6d3a99a40fc24db34efb014f42c4834
Caleb Winters <calebw@missionreadyhq.com>
2022-09-22 18:16:07 +1200

    Recent update to page

---
+++ client/src/components/PropertyFeatureComponents/PropertyPics.js
@@ -0,0 +1,22 @@
+import '../../style/FeaturePageStyles/propertyPics.css';
+import { useState } from "react"
+import { useEffect } from 'react';
+
+
+export default function PropertyPics() {
+
+    return(
+        <div className="Main-Body-Pics">
+
+        {/* {results &&
+            results.map((result) => (    
+            <div className="Image-Cards">
+                       
+                     <img src={result.pictures}/>          
+            
+            </div>
+         ))} */}
+        </div>
+     
+    )
+}
