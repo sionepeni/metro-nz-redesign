a10ed05fbde7c87115262335af4b530ab3cda296
Wtipene <Wilemtipene@gmail.com>
2022-09-23 08:52:11 +1200

    fixed npm issue

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,3 @@
-import {useContext} from 'react'
-import AppContext from '../context/AppContext'
-
 import Header from '../components/header/Header.js'
 import Footer from '../components/footer/Footer.js'
 import HomeHeroImg from '../components/HomeHeroImg.js'
@@ -8,7 +5,6 @@ import HomeSearch from '../components/HomeSearch.js'
 import HomeMiddleSection from '../components/HomeMiddleSection.js'
 import HomeTenants from '../components/HomeTenants.js'
 import HomeWhyUs from '../components/HomeWhyUs.js'
-import ReviewCards from '../components/ReviewCards'
 import HomeCards from '../components/HomeCards'
 
 
